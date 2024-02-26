e9f855bf1c39c97268c2023e5d5e61c5c42a894d
Lukasz <lukasz.zatyka@gmail.com>
2024-02-26 12:45:45 +0100

    Prepared tailwind config

---
+++ next-app/src/app/terms.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from 'next';
+import * as React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Terms',
+};
+
+export default function Terms() {
+  return (
+    <main>
+      <section className='flex items-center p-16 h-[calc(100vh - 72px)]'>
+        <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+          <div className='max-w-md text-center'>
+            <h2 className='mb-8 font-bold text-9xl text-primary'>
+              <span className='sr-only'>Terms</span>
+            </h2>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
--- next-app/src/components/navbar/Navbar.tsx
+++ next-app/src/components/navbar/Navbar.tsx
@@ -1,10 +1,12 @@
-"use client"
+'use client';
 
 import Link from 'next/link';
 import { useState } from 'react';
 
+import Button from '../buttons/Button';
+
 export default function Navbar() {
-  const [menuIcon, setIcon] = useState <boolean>(false);
+  const [menuIcon, setIcon] = useState<boolean>(false);
 
   const handleSmallerScreenNavigation = () => {
     setIcon(!menuIcon);
@@ -14,9 +16,18 @@ export default function Navbar() {
       <nav className='max-w-[1366px] mx-auto h-[100px] flex justify-between items-center p-4'>
         <div>
           <Link href='/'>
-            <span className='text-3xl font-bold font-extrabold'>Next Qwik</span>
+            <span className='text-3xl font-extrabold'>Next Qwik</span>
           </Link>
         </div>
+        <ul className='hidden md:flex font-semibold text-1xl lg:text-[20px] text-slate-800'>
+          <li className='mr-4 lg:mr-8 hover:text-black'>
+            <Link href='/home'>Pages</Link>
+          </li>
+          <li className='mr-4 lg:mr-8 hover:text-black'>
+            <Link href='/terms'>Terms</Link>
+          </li>
+        </ul>
+        <Button className="hidden md:flex">Download</Button>
       </nav>
     </header>
   );
